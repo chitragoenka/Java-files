6c06bda0d0ef78c6a3f124fea037ec2ad0511c72
chitragoenka <chitra.goenka@gmail.com>
2023-05-10 17:01:56 -0400

    Added more files

---
+++ src/com/example/ArraysListsPractice/SumOfList.java
@@ -0,0 +1,32 @@
+package com.example.ArraysListsPractice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SumOfList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter an integer (or 0 to end): ");
+            int input = scanner.nextInt();
+            if (input == 0) {
+                break;
+            }
+            numbers.add(input);
+        }
+
+        System.out.print(numbers.toString().replace("[","").replace("]","") + " were the items in the list. ");
+        System.out.println("The sum of that list is:\n" + sumList(numbers));
+    }
+
+    public static int sumList(ArrayList<Integer> list) {
+        int sum = 0;
+        for (int number : list) {
+            sum += number;
+        }
+        return sum;
+    }
+}
+
